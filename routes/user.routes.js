f69b171da2400fe72092cb531b3914c635fd3b35
Azure <paramasuraqutay@gmail.com>
2025-10-05 01:18:10 +0800

    update models, database, routes

---
+++ routes/user.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const userRouter = Router();
+
+userRouter.get('/', (req, res) => res.send({title: 'GET all users'}));
+
+userRouter.get('/:id', (req, res) => res.send({title: 'GET users details'}));
+
+userRouter.post('/', (req, res) => res.send({title: 'CREATE all users'}));
+
+userRouter.put('/', (req, res) => res.send({title: 'UPDATE all users'}));
+
+userRouter.delete('/', (req, res) => res.send({title: 'DELETE all users'}));
+
+export default userRouter;
